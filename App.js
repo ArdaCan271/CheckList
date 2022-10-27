22ff6143135147cf6ea9cb7d8aa272ddbd02dfa8
U210709027 <ardacan271@gmail.com>
2022-10-27 18:07:01 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <SafeAreaView style={backgroundStyle}>
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  }
+});
+
+export default App;
---
+++ screens/HomePage.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native"
+
+const HomePage = () => {
+  return (
+    <View>
+
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  }
+});
